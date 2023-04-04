b251906f04f28379c1060dd1656f9330a68db86a
MorganLevetti <113036290+MorganLevetti@users.noreply.github.com>
2023-04-04 11:37:23 +0200

    ajout de commentaires

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
@@ -7,20 +6,20 @@ use App\Http\Controllers\UsersController;
 use App\Http\Controllers\ContactController;  
 use App\Http\Controllers\PhotoController;
 
-
+// Home
 Route::get('/', [WelcomeController::class, 'index']);
-
+// Articles
 Route::get('/article/{n}', [ArticleController::class, 'show'])->where('n', '[0-9]+');
-
+// Utilisateur
 Route::get('users', [UsersController::class, 'create']);
 Route::post('users', [UsersController::class, 'store']);
-
+// Systeme de contact
 Route::get('contact', [ContactController::class, 'create']);
 Route::post('contact', [ContactController::class, 'store']);
-
+// Systeme ajout photo
 Route::get('photo', [PhotoController::class, 'create']);
 Route::post('photo', [PhotoController::class, 'store']);
-
+// Systeme de facture
 Route::get('facture/{n}', function($n) {
     return view('facture')->with('numero', $n);
 })->where('n', '[0-9]+');
